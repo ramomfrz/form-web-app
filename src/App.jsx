13519e9612389c1adeda3d5aa98cc4c4042432e7
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-27 02:16:56 -0300

    Build: Corpo do projeto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css"
+import React from "react"
+import Form from "./components/Form/form"
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form/dados/dados.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function dados() {
+    return (
+        <div>dados</div>
+    )
+}
+
+export default dados
