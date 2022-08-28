7724aafb7673f014a34d69b4c4882a241044dcc5
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-28 01:01:34 -0300

    Build: Cria componente de endereço com inputs

---
+++ src/components/Form/dados/endereco.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function endereco() {
+    return (
+        <div className='endereco-container'>
+            <input type="text" placeholder="CEP" />
+            <input type="text" placeholder="Endereço" />
+            <input type="text" placeholder="Endereço 2" />
+        </div>
+    )
+}
+
+export default endereco
