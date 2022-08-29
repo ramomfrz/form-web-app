24d05962d1747d60a23d920b7e185fc1557a3e3b
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-29 18:48:51 -0300

    Build: Cria componente de pagina inicial

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home-container'>
+            <h2>Não perca tempo, cadastre seus clientes!</h2>
+        </div>
+    )
+}
+
+export default Home
