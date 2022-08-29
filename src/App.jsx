2521361200f5c8da1248cdb9bc4c6a958b3e5339
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-29 18:36:32 -0300

    Build: Cria o APP com seus componentes e rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import "./App.css"
-import React from "react"
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import React, { useState } from "react"
 import Form from "./components/Form/form"
-
+import Home from "./components/Home/"
+import Sidebar from './components/sidebar/'
+import Clientes from './components/Clientes/clientes'
+import "./App.css"
 
 function App() {
+  const [page, setPage] = useState(0)
+  const [clients, setClients] = useState([])
+  const [formDados, setFormDados] = useState([]);
+  const FormTitles = ["Nos informe alguns dados", "onde podemos te encontrar?", "E por último..."];
 
   return (
     <div className="App">
-      <Form />
+      <BrowserRouter>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="form" element={<Form page={page} setPage={setPage} formDados={formDados} setFormDados={setFormDados} FormTitles={FormTitles} clients={clients} setClients={setClients} />} />
+          <Route path="clientes" element={<Clientes formDados={clients} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
