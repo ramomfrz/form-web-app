b1dc37e0516565b51fabffeeab32cc7bcc7245b6
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-29 18:41:26 -0300

    Build: Cria o componente de endereço

--- src/components/Form/dados/endereco.jsx
+++ src/components/Form/dados/endereco.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
-function endereco() {
+function endereco({ formDados, setFormDados }) {
     return (
         <div className='endereco-container'>
-            <input type="text" placeholder="CEP" />
-            <input type="text" placeholder="Endereço" />
-            <input type="text" placeholder="Endereço 2" />
+            <label>CEP:</label>
+            <input type="text" placeholder="CEP" value={formDados.cep} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    cep: event.target.value
+                }
+            })} />
+            <label>Endereço:</label>
+            <input type="text" placeholder="Endereço" value={formDados.endereco} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    endereco: event.target.value
+                }
+            })} />
+            <label>Endereço 2:</label>
+            <input type="text" placeholder="Endereço 2" value={formDados.endereco2} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    endereco2: event.target.value
+                }
+            })} />
         </div>
     )
 }
