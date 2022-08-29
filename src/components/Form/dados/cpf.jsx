49844ab3d0c59faded650b153a16c51db45d7b04
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-29 18:37:34 -0300

    Build: cria componente de cpf para o formulário

--- src/components/Form/dados/cpf.jsx
+++ src/components/Form/dados/cpf.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 
-function cpf() {
+function cpf({ formDados, setFormDados }) {
     return (
         <div className='cpf-container'>
             <div className="nome-container">
-                <input type="text" placeholder="__/__/____" />
-                <input type="text" placeholder="CPF" />
-                <input type="text" placeholder="Renda Mensal" />
+                <label>Data de nascimento:</label>
+                <input type="text" placeholder="(__/__/____)" value={formDados.data} onChange={(event) => setFormDados((prev) => {
+                    return {
+                        ...prev,
+                        data: event.target.value
+                    }
+                })} />
+                <label>CPF:</label>
+                <input type="text" placeholder="CPF" value={formDados.cpf} onChange={(event) => setFormDados((prev) => {
+                    return {
+                        ...prev,
+                        cpf: event.target.value
+                    }
+                })} />
+                <input type="text" placeholder="Renda Mensal" value={formDados.renda} onChange={(event) => setFormDados((prev) => {
+                    return {
+                        ...prev,
+                        renda: event.target.value
+                    }
+                })} />
             </div>
         </div>
     )
