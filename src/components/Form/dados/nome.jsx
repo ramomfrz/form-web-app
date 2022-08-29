8315e72bcf1e4ee85e712a5e204d237392e6d39d
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-29 18:48:23 -0300

    Build: Cria componente 'Nome'

--- src/components/Form/dados/nome.jsx
+++ src/components/Form/dados/nome.jsx
@@ -3,12 +3,36 @@ import React from 'react'
 function Nome({ formDados, setFormDados }) {
     return (
         <div className="nome-container">
-            <input type="text" placeholder="Nome" value={formDados.nome} onChange={(event) => setFormDados({ ...formDados, nome: event.target.value })} />
-            <input type="text" placeholder="Sobrenome" value={formDados.sobrenome} onChange={(event) => setFormDados({ ...formDados, sobrenome: event.target.value })} />
-            <input type="text" placeholder="seuemail@exemplo.com" value={formDados.email} onChange={(event) => setFormDados({ ...formDados, email: event.target.value })} />
-            <input type="text" placeholder="(00) 00000-0000" value={formDados.telefone} onChange={(event) => setFormDados({ ...formDados, telefone: event.target.value })} />
+            <label>Nome:</label>
+            <input type="text" placeholder="Nome" value={formDados.nome} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    nome: event.target.value
+                }
+            })} />
+            <label>Sobrenome:</label>
+            <input type="text" placeholder="Sobrenome" value={formDados.sobrenome} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    sobrenome: event.target.value
+                }
+            }
+            )} />
+            <label>E-mail:</label>
+            <input type="text" placeholder="seuemail@exemplo.com" value={formDados.email} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    email: event.target.value
+                }
+            })} />
+            <label>Telefone:</label>
+            <input type="text" placeholder="(00) 00000-0000" value={formDados.telefone} onChange={(event) => setFormDados((prev) => {
+                return {
+                    ...prev,
+                    telefone: event.target.value
+                }
+            })} />
         </div>
     )
 }
-
 export default Nome
