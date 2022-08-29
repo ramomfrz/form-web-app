54e5e7ecf0ce93859da0ca3f61923bb264f53740
Ramom Ferraz <ramomferraz@hotmail.com>
2022-08-29 18:49:15 -0300

    Build:  Cria botões para cadastro e visualização de clientes

---
+++ src/components/sidebar/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Sidebar() {
+    return (
+        <div className='sidebar-container'>
+
+            <nav>
+                <div className='HomeSideBar'>
+                    <Link to="/"><p>Home</p></Link>
+                </div>
+                <div className='teste'>
+                    <div className='box-input'>
+                        <Link to="form"><a>Cadastrar cliente</a></Link> <br />
+                    </div>
+                    <div className='box-input'>
+                        <Link to="clientes"><a>Clientes cadastrados</a></Link>
+                    </div>
+                </div>
+            </nav >
+
+        </div >
+    )
+
+}
+export default Sidebar
